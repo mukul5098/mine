70a07699d5f1f3e19b0ffd062b13218d2d78e034
mukul kumar <mukul.kumar.phe14@itbhu.ac.in>
2017-07-31 20:13:33 +0000

    initial file commit

---
+++ first.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int add(int a, int b);
+
+int main(void) 
+
+{
+    int a = GetInt();
+    int b = GetInt();
+    printf("sum = %i \n", &n);
+}
+
+int add(int a, int b)
+{
+    int sum = a + b ;
+    return sum ;
+}
